82a7637a4b3494180aef983c702504ef85ea35c2
vlshutyak <vl.shutyak@gmail.com>
2023-04-12 17:52:51 +0300

    stage0-m2-t3 var name change

--- src/main/java/lang/print/gaps/task2/JavaVariableNames.java
+++ src/main/java/lang/print/gaps/task2/JavaVariableNames.java
@@ -3,12 +3,12 @@ package lang.print.gaps.task2;
 public class JavaVariableNames {
     public static void main(String[] args) {
         int tomorrow = 0;
-        int $money = 10;
+        int money = 10;
         int hundred = 100;
         int back = 1000;
         int underscore = 10000;
         int sharp = 100000;
         int star = 1000000;
-        final int  BAD_EXAMPLE_OF_VAR_NAME = 1000000;
+        int badExampleOfVarName = 1000000;
     }
 }
